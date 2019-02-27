0bbf87392eb05a5bf8a7a389d3c09273b0f9cdad
Sciré  Jérémie <scirej@bs-mbpr261.d.ethz.ch>
2019-02-27 15:53:01 +0100

    Reverted check on number of types in HiddenParameterization, was an error while rebasing

--- src/bdmm/parameterization/HiddenParameterization.java
+++ src/bdmm/parameterization/HiddenParameterization.java
@@ -37,9 +37,6 @@ public class HiddenParameterization extends CanonicalParameterization {
 
 		typeSet = typeSetInput.get();
 		nTypes = typeSet.getNTypes();
-		if(nTypes % 2 != 0)
-			throw new IllegalArgumentException("nTypes input must be an even number. nTypes should be twice the number of observed types, regardless of how which of the possible hidden types are allowed.");
-
 		nObsTypes = nTypes/2; // initialize with all hidden types
 
 		isMigRateSymmetric = new boolean[nObsTypes];
